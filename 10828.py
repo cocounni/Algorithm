30d84854afa7a8735abded9860bb87beea0a8460
cocounni <80098315+cocounni@users.noreply.github.com>
2023-04-16 17:37:28 +0900

    백준 10828 스택

---
+++ 10828.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for i in range(n):
+    text = sys.stdin.readline().split()
+
+    if text[0] == 'push':
+        stack.append(text[1])
+    elif text[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif text[0] == 'size':
+        print(len(stack))
+    elif text[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+    elif text[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
