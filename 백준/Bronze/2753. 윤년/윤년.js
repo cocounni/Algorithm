bb89bb886e0ff4a37e9aa4beb3f8cf53583fefc4
cocounni <80098315+cocounni@users.noreply.github.com>
2024-01-15 18:54:59 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString();
+
+const year = parseInt(inputData);
+
+if (year % 4 == 0 && year % 100 != 0) {
+    console.log('1');
+} else if (year % 400 == 0) {
+    console.log('1');
+} else {
+    console.log('0');
+}
